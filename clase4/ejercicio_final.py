09d59a7917faef873b97bc8f2ba4ab1084eb703d
jose.hermosillaa <jose.hermosillaa@usach.cl>
2026-01-13 22:56:36 -0300

    se agrega resolucion del ejercicio final

--- clase4/ejercicio_final.py
+++ clase4/ejercicio_final.py
@@ -9,16 +9,34 @@ class Producto:
 radio = Producto("Radio","Audio","dispositivo para escuchar musica", True)
 lavadora = Producto("Lavadora","electrodomestico","dispositivo para lavar la ropa", True)
 celular = Producto("Celular","telefono","dispositivo para llamar", True)
+audifono = Producto("Audifono","accesorio","dispositivo para escuchar musica en movimiento", False)
 
-#por que el atributo disponible marca si hay o no stock del producto en la tienda
-if radio.disponible == True:
-    ### if radio.categoria ==electrodomestico
-    ### mensaje si es electrodomestico y la descripcion
-    ### if radio.categoria == telefono
-    ### if radio.categoria == computadora
-    ### if radio.categoria == accesorio
-    ### else: 
-    ### no se encontro categoria para el producto
-    print("aqui escribir su codigo")
-else:
-    print(f"el producto {radio.nombre} se encuentra agotado")
+
+#lista de los productos
+productos = [ radio, lavadora, celular, audifono]
+
+for producto in productos:
+
+    #por que el atributo disponible marca si hay o no stock del producto en la tienda
+    if producto.disponible == True:
+        
+        if producto.categoria == "electrodomestico":
+            print("el producto pertenece a la categoria electrodomestico")
+            print(f"Descripcion: {producto.descripcion}")
+            
+        elif producto.categoria == 'telefono':
+            print("el producto pertenece a la categoria teléfono")
+            print(f"Descripcion: {producto.descripcion}")
+
+        elif producto.categoria == 'computadora':
+            print("el producto pertenece a la categoria computadora")
+            print(f"Descripcion: {producto.descripcion}")
+
+        elif producto.categoria == 'accesorio':
+            print("el producto pertenece a la categoria accesorio")
+            print(f"Descripcion: {producto.descripcion}")
+        else:
+            print("El producto no pertenece a ningua de las categorias del sistema (electrodomestico, telefono, computadora, accesorio)")
+
+    else:
+        print(f"el producto {producto.nombre} se encuentra agotado")
